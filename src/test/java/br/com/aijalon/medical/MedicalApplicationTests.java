891d2be17a63028896861692dd05ae915c3f0d0f
aijalonti <aijalon.ti@gmail.com>
2021-05-01 19:42:48 -0300

    feat: first commit project

---
+++ src/test/java/br/com/aijalon/medical/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aijalon.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
